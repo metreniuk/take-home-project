53c258941d451dc7b734c39dc04dfc8fb4486358
Alexandr Metreniuc <metreniuk@gmail.com>
2022-12-18 16:16:56 +0200

    refactor code

--- dpk.js
+++ dpk.js
@@ -1,28 +1,53 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+/**
+ * Refactoring:
+ *
+ * - Extracted the hashing function to make it easier to test
+ * - The base case uses the hashing function to create the partition key
+ * - Each edge case is extracted to a guard statement
+ * - Prefer to not edge cases that have similar conditions but still are different
+ *
+ */
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+function deterministicPartitionKey(event, hashKeyFn = hashKey) {
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (
+    event.partitionKey &&
+    typeof event.partitionKey === "string" &&
+    event.partitionKey.length < MAX_PARTITION_KEY_LENGTH
+  ) {
+    return event.partitionKey;
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+
+  if (
+    event.partitionKey &&
+    JSON.stringify(event.partitionKey).length < MAX_PARTITION_KEY_LENGTH
+  ) {
+    return JSON.stringify(event.partitionKey);
   }
-  return candidate;
+
+  const hashableData = event.partitionKey ?? event;
+  const stringifiedData =
+    typeof hashableData !== "string"
+      ? JSON.stringify(hashableData)
+      : hashableData;
+
+  return hashKeyFn(stringifiedData);
+}
+
+function hashKey(key) {
+  return crypto.createHash("sha3-512").update(key).digest("hex");
+}
+
+module.exports = {
+  deterministicPartitionKey,
+  TRIVIAL_PARTITION_KEY,
+  MAX_PARTITION_KEY_LENGTH,
 };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,51 @@
-const { deterministicPartitionKey } = require("./dpk");
+const {
+  deterministicPartitionKey,
+  TRIVIAL_PARTITION_KEY,
+  MAX_PARTITION_KEY_LENGTH,
+} = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
+  it(`Returns the literal '${TRIVIAL_PARTITION_KEY}' when given no input`, () => {
+    const expectedKey = "0";
+    const actualKey = deterministicPartitionKey();
+
+    expect(actualKey).toBe(expectedKey);
+  });
+
+  it(`Returns the partitionKey from the event if its length is less than ${MAX_PARTITION_KEY_LENGTH}`, () => {
+    const expectedKey = "test partition key";
+    const event = { partitionKey: expectedKey };
+    const actualKey = deterministicPartitionKey(event);
+
+    expect(actualKey).toBe(expectedKey);
+  });
+
+  it(`Returns the partitionKey from the event converted to string`, () => {
+    const event = { partitionKey: 1231 };
+    const expectedKey = "1231";
+    const actualKey = deterministicPartitionKey(event);
+
+    expect(actualKey).toBe(expectedKey);
+  });
+
+  it(`Returns the hash of the partitionKey from the event if its length is greater than ${MAX_PARTITION_KEY_LENGTH}`, () => {
+    const expectedKey = "test key";
+    const hashMock = () => expectedKey;
+    const testPartitionKey = "*".repeat(MAX_PARTITION_KEY_LENGTH + 1);
+    const event = { partitionKey: testPartitionKey };
+    const actualKey = deterministicPartitionKey(event, hashMock);
+
+    expect(actualKey).not.toBe(testPartitionKey);
+    expect(actualKey.length).toBeLessThan(MAX_PARTITION_KEY_LENGTH);
+    expect(actualKey).toBe(expectedKey);
+  });
+
+  it(`Returns the hashed event if it doesn't have a partitionKey`, () => {
+    const expectedKey = "test key";
+    const hashMock = () => expectedKey;
+    const event = { randomTestKey: 1231 };
+    const actualKey = deterministicPartitionKey(event, hashMock);
+
+    expect(actualKey).toBe(expectedKey);
   });
 });
